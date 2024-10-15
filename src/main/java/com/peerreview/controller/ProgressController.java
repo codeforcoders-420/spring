19c2906b7ef7243e05e67ac0ec33990d5bd3ca27
codeforcoders-420 <javacodeforlife420@gmail.com>
2024-10-15 15:58:38 -0400

    Latest

--- src/main/java/com/peerreview/controller/ProgressController.java
+++ src/main/java/com/peerreview/controller/ProgressController.java
@@ -1,5 +1,16 @@
-package com.peerreview.controller;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ProgressController {
 
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public ProgressController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    public void sendProgressUpdate(double percentage) {
+        messagingTemplate.convertAndSend("/topic/progress", percentage);
+    }
 }
